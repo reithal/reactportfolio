1ee20378a01f1fdbd221103e5b5b2c7a3528e5b7
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-06-08 23:44:35 -0400

    Updates

---
+++ precache-manifest.f82a86bef9e02cbde2a04567d76ef858.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fcad9ebedcbaaab57df",
+    "url": "/reactportfolio/static/css/main.d3fc652a.chunk.css"
+  },
+  {
+    "revision": "8fcad9ebedcbaaab57df",
+    "url": "/reactportfolio/static/js/main.60cd1185.chunk.js"
+  },
+  {
+    "revision": "8007ccf6b0ab45eb369a",
+    "url": "/reactportfolio/static/js/runtime~main.d7794af4.js"
+  },
+  {
+    "revision": "62bd7c3533b8dade16d9",
+    "url": "/reactportfolio/static/js/2.f1dc5928.chunk.js"
+  },
+  {
+    "revision": "ceb15a711ccbd64fce704bb5c576504b",
+    "url": "/reactportfolio/static/media/hd1484.ceb15a71.mov"
+  },
+  {
+    "revision": "c45720d25b09fdb247dbd61cec31d5ac",
+    "url": "/reactportfolio/static/media/IPAAnon.c45720d2.png"
+  },
+  {
+    "revision": "8d9e1db3bdc9d023f0af5a1ce7367ecd",
+    "url": "/reactportfolio/static/media/JSQuiz.8d9e1db3.png"
+  },
+  {
+    "revision": "82005baf01bd617a567bb58a4539e7ff",
+    "url": "/reactportfolio/static/media/LocalOneOff.82005baf.png"
+  },
+  {
+    "revision": "40599fb015b96de2524e2b11b868830d",
+    "url": "/reactportfolio/static/media/PWGenerator.40599fb0.png"
+  },
+  {
+    "revision": "6972f308a9b13df5dd18a58eb6a57961",
+    "url": "/reactportfolio/static/media/WeatherDashboard.6972f308.png"
+  },
+  {
+    "revision": "fc8e0dacd53fc00f217b7b4cca30f3fc",
+    "url": "/reactportfolio/static/media/employeetracker.fc8e0dac.gif"
+  },
+  {
+    "revision": "746a027c6f8caf9fd1fc44f12a1516d7",
+    "url": "/reactportfolio/static/media/cover.746a027c.png"
+  },
+  {
+    "revision": "5e4de25f3ed6e20dcee4ec0e63cd2587",
+    "url": "/reactportfolio/index.html"
+  }
+];
